28d51342d890879515bd12ff8dbba1cdc03b99f8
root <hao_xiaosa@qq.com>
2024-03-31 16:45:07 +0800

    version 2.0.0 update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hahahaha");
         System.out.println("Hello world!");
+        System.out.println("version 2.0.0");
     }
 }
